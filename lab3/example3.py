f454989e6fff216c5a9192275b4660f90d0c1433
x59272796 <yigitdurmaz@std.iyte.edu.tr>
2020-10-26 09:01:43 +0000

    example3 reworked

--- lab3/example3.py
+++ lab3/example3.py
@@ -1,12 +1,14 @@
 GPA = float (input("insert your GPA"))
 numberOfLectures = int (input("insert your number of lectures."))
-if GPA < 2.0 :
-  if numberOfLectures < 47 :
-    print("not enough gpa and lectures!")
- else :
-   print("not enough gpa")
-elif GPA >= 2.0:
-  if numberOfLectures < 47 :
-    print("not enough lectures")
-  else :
-    print("congratulations! you've been promoted to unemployed.")        
+if  GPA < 2.0 :
+  if  numberOfLectures < 47 :
+    print("not enough GPA and lectures.")
+  else  :
+    print("not enough GPA.")
+else  :
+  if  numberOfLectures < 47 :
+    print("not enough lectures.")
+  else  :
+    if  GPA > 4.0 :
+      print("woah, i never saw a GDP higher than 4.0 before...")
+    print("congratulations, you've been promoted to unemployed!!!")             
