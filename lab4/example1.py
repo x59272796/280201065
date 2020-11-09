eddc2bda419defea9225002f18a22f0dc6572211
x59272796 <yigitdurmaz@std.iyte.edu.tr>
2020-11-09 09:00:14 +0000

    refined statements in example1

--- lab4/example1.py
+++ lab4/example1.py
@@ -2,9 +2,9 @@ number = int(input("insert a number"))
 requiredDigits = number % 100
 if  requiredDigits == 0 :
   print(0)
-elif requiredDigits < 10  :
+elif requiredDigits < 10  and requiredDigits != 0 :
   print (requiredDigits)  
-elif requiredDigits < 100 :
+elif requiredDigits < 100 and requiredDigits >= 10  :
   tens = requiredDigits // 10
   ones = requiredDigits % 10
   sum = tens + ones
