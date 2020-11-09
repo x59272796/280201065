b5b9d4aec0b5dd2e1765c72f12cc556bf78f2260
x59272796 <yigitdurmaz@std.iyte.edu.tr>
2020-11-09 09:06:25 +0000

    added a space

--- lab4/example2.py
+++ lab4/example2.py
@@ -1,4 +1,4 @@
-year = int(input("please insert a year"))
+year = int(input("please insert a year. "))
 if year % 4 == 0  :
   if year % 100 == 0 and year % 400 != 0  :
     print (str(year) + " is not a leap year.")
