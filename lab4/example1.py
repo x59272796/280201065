57f69c93807245741dd4e8b95a5c7ed3a20e0793
x59272796 <yigitdurmaz@std.iyte.edu.tr>
2020-11-09 08:58:53 +0000

    implemented if else statements to example1

--- lab4/example1.py
+++ lab4/example1.py
@@ -1,6 +1,11 @@
 number = int(input("insert a number"))
 requiredDigits = number % 100
-tens = requiredDigits // 10
-ones = requiredDigits % 10
-sum = tens + ones
-print(sum)
+if  requiredDigits == 0 :
+  print(0)
+elif requiredDigits < 10  :
+  print (requiredDigits)  
+elif requiredDigits < 100 :
+  tens = requiredDigits // 10
+  ones = requiredDigits % 10
+  sum = tens + ones
+  print(sum)
