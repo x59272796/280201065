be12c312d2d2ddc66e93bf8b9e97d3c07754ea1f
x59272796 <yigitdurmaz@std.iyte.edu.tr>
2020-11-16 09:37:07 +0000

    finalised example 3

--- lab5/example3.py
+++ lab5/example3.py
@@ -4,15 +4,15 @@ while a <= 0 :
   a = int(input("insert the first positive integer. "))
 while b <= 0 :
   b = int(input("insert the second positive integer. "))
-c = 10
+c = 1
 sameDigits = 0
 if a > b :
   length = len(str(b))
 else :
   length = len(str(a))
 for x in range(length) :
-  remainder1 = a % c 
-  remainder2 = b % c
+  remainder1 = (a // c) % 10
+  remainder2 = (b // c) % 10
   if remainder1 == remainder2 :
     sameDigits += 1
   c *= 10
