420828c25c382292cce7c86a9a328470d231b954
x59272796 <yigitdurmaz@std.iyte.edu.tr>
2020-12-14 09:26:15 +0000

    added example3

---
+++ lab8/example3.py
@@ -0,0 +1,22 @@
+import random
+def get_rand_list(a,b,c) :
+  randList = []
+  for x in range(c) :
+    d = random.randint(a,b)
+    while d in randList : 
+      d = random.randint(a,b)
+    randList.append(d)
+  return randList
+def getoverlap(a,b) :
+  intersection = []
+  for element in a :
+    if element in b :
+      intersection.append(element)
+  return intersection
+def main() :
+  firstList = get_rand_list(0,10,5)
+  secondList = get_rand_list(0,10,5)
+  print(getoverlap(firstList, secondList))
+  print(firstList)
+  print(secondList)
+main()
