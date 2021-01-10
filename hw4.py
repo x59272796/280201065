e787234b81ec7bf5ad090f03299a35b29dc3a273
x59272796 <yigitdurmaz@std.iyte.edu.tr>
2021-01-10 15:27:20 +0000

    uploaded final version of hw4

--- hw4.py
+++ hw4.py
@@ -1,13 +1,15 @@
+#280201065
+
 def printStatus(task_list, stats, heroHP, pegasusHP) : # prints current status and remaining tasks
   printHP(heroHP, pegasusHP)
   print("Here are the tasks left that hero needs to complete: ")
-  print("--------------------------------------------------------")
+  print("----------------------------------------------------")
   print("| TaskName | ByFootDistance | ByPegasus | HPNeeded |")
-  print("--------------------------------------------------------")
-  getTask(task_list, stats, 0) # recursive function that prints remaining tasks
-  print("--------------------------------------------------------")
+  print("----------------------------------------------------")
+  print_remaining_tasks(task_list, stats, 0) # recursive function that prints remaining tasks
+  print("----------------------------------------------------")
 
-def printLine(task_list, stats, a, b) : # recursive function that returns lines, adjusts line size and extras to add such as km 
+def printLine(task_list, stats, a, b) : # recursive function that returns lines, adjusts line size and extras to add such as km, stats is used to determine the length of each row entry, a is task index and b is index of each attribute of that task
   string = ""
   string += "| " + task_list[a][b]  # add the first entry which is TaskName
   if b == 1 or b == 2 : # add km if its the distance column
@@ -20,31 +22,31 @@ def printLine(task_list, stats, a, b) : # recursive function that returns lines,
     return string + "|"
   return string
 
-def printHall (hallstats, hallList, a, table, repeat) :
+def printHall (hallstats, hallList, player, table, repeat) : # prints the hall of fame with the specified repeat time, hallstats helps me determine the length of the table, hallList is the list with the high scores and players, player is entry number of a player, table is if we are first printing "hall of fame" and name/finish time, repeat is the number of players to show in the table
   string = ""
-  if table :
-    print("\n          Hall of Fame          ")
+  if table : #use it when printing the table for the first time
+    print("\n          Hall of Fame          ") # header
     print("--------------------------------")
     print("| Name           | Finish Time |")
     print("--------------------------------")
-  string += "| " + hallList[a][0] + (len(hallstats[0]) - len(hallList[a][0]) - 5) * " "
-  string += " | " + hallList[a][1] + (len(hallstats[1]) - len(hallList[a][1]) - 4) * " " + " |"
+  string += "| " + hallList[player][0] + (len(hallstats[0]) - len(hallList[player][0]) - 5) * " " # like with the function above, aligns the text
+  string += " | " + hallList[player][1] + (len(hallstats[1]) - len(hallList[player][1]) - 4) * " " + " |"
   print(string)
   print("--------------------------------")
   repeat -= 1
-  if hallList[a] != hallList[-1] and repeat > 0 :
-    printHall(hallstats, hallList, a+1, False, repeat)
+  if hallList[player] != hallList[-1] and repeat > 0 :
+    printHall(hallstats, hallList, player+1, False, repeat) # print the next entry in a recursive manner
 
-def getTask(task_list, stats, a) : # get each task and print them in a line
+def print_remaining_tasks(task_list, stats, a) : # get each task and print them in a line, stats is used to get the length of each row entry and a is used to get the index of task
   print(printLine(task_list, stats, a, 0))
   if task_list[a] != task_list[-1] :
-    getTask(task_list, stats, a+1)
+    print_remaining_tasks(task_list, stats, a+1) # recursive part
 
-def printHP(heroHP, pegasusHP) :
+def printHP(heroHP, pegasusHP) : # prints hps of hero and pegasus
   print("Remaining HP for Hero : " + str(heroHP))
   print("Remaining HP for Pegasus: " + str(pegasusHP) + "\n")
 
-def askDestination(task_dict, heroHP) :
+def askDestination(task_dict, heroHP) : #asks where to go
   while True :
     destination = str(input("Where should Hero go next? ")).lower().capitalize()
     if destination in task_dict.keys() :
@@ -52,12 +54,12 @@ def askDestination(task_dict, heroHP) :
     else :
       print("Invalid input")
   if int(task_dict[destination][2]) > heroHP :
-    destination == "heaven"
+    destination == "heaven" # heaven is a silly way to sign that the game over conditions are fulfilled
   return destination
 
-def askTravel(task_dict, destination, heroHP, heroSpeed, heroHPLose, pegasusHP, pegasusSpeed, pegasusHPLose, string) :
-  if (int(task_dict[destination][0]) == -1 and pegasusHP < ((int(task_dict[destination][1])/pegasusSpeed) * pegasusHPLose)) or (int(task_dict[destination][2]) > heroHP) or ((int(task_dict[destination][0]) / heroSpeed) * heroHPLose) > heroHP :
-    travel = "dead"
+def askTravel(task_dict, destination, heroHP, heroSpeed, heroHPLose, pegasusHP, pegasusSpeed, pegasusHPLose, string) : #ask how to go to a destination and determine if the game will continue, parameters are kind of self explanatory, string is the question where we ask the player how to go home or to the quest
+  if (int(task_dict[destination][0]) == -1 and pegasusHP < ((int(task_dict[destination][1])/pegasusSpeed) * pegasusHPLose)) or (((int(task_dict[destination][0]) / heroSpeed) * heroHPLose) > heroHP and pegasusHP < ((int(task_dict[destination][1])/pegasusSpeed) * pegasusHPLose)) : # fail conditions, if the destination is unreachable on foot and pegasus has insufficient hp OR both the hero and pegasus have insufficient hp to travel
+    travel = "dead" # dead is a silly way to sign that the game is over.
   else :
     while True :
       travel = str(input(string)).lower().capitalize()
@@ -76,14 +78,14 @@ def askTravel(task_dict, destination, heroHP, heroSpeed, heroHPLose, pegasusHP, 
         print("Invalid input")
   return travel
 
-def calculateTime(task_dict, destination, travel, heroSpeed, pegasusSpeed) :
+def calculateTime(task_dict, destination, travel, heroSpeed, pegasusSpeed) : # calculate the time with these given parameters
   if travel == "Pegasus" :
     timeElapsed = int(task_dict[destination][1]) / pegasusSpeed
   else :
     timeElapsed = int(task_dict[destination][0]) / heroSpeed
   return int(timeElapsed)
 
-def calculateHP(task_dict, destination, travel, quest, heroHP, pegasusHP, pegasusHPLose, heroHPLose, timeElapsed) :
+def calculateHP(task_dict, destination, travel, quest, heroHP, pegasusHP, pegasusHPLose, heroHPLose, timeElapsed) : # calculate the remaining hp, quest is if the hero has fought a monster or not
   if travel == "Pegasus" :
     pegasusHP -= pegasusHPLose * timeElapsed
   else :
@@ -92,14 +94,14 @@ def calculateHP(task_dict, destination, travel, quest, heroHP, pegasusHP, pegasu
     heroHP -= int(task_dict[destination][2])
   return heroHP, pegasusHP
 
-def removeTask(task_list, task_dict, destination, n) :
+def removeTask(task_list, task_dict, destination, n) : #remove tasks in a recursive manner
   if task_list[n][0] == destination :
     task_list.pop(n)
     task_dict.pop(destination)
   else :
     removeTask(task_list, task_dict, destination, n+1)
 
-def sortList(hallList) :
+def sortList(hallList) : # a function to sort a list with their second entry. instead of using a lambda key inside sort(), i felt more comfortable implementing a function like that
   scoreList = []
   sortedList = []
   for score in hallList :
@@ -112,25 +114,27 @@ def sortList(hallList) :
   return sortedList
 
 def main() :
+  # we initialize the game from here to the starting string
   file = open("TaskList.txt", "r", encoding = "utf-8")
+  hall_of_fame_location = "hall_of_fame.txt" # modify this string to change the hall of fame file location
   task_list = []
   task_dict = {}
-  for line in file :
+  for line in file : # get each task to a list and a dictionary
     line = line.replace("\n", "").split(",")
     task_list.append(line)
     task_dict[line[0]] = [line[1], line[2], line[3]]
   file.close()
-  file = open("hall_of_fame.txt", "a", encoding = "utf-8") # to make sure that a hall of fame file exists
+  file = open(hall_of_fame_location, "a", encoding = "utf-8") # to make sure that a hall of fame file exists, appending creates the file if it doesnt exist
   file.close()
-  file = open("hall_of_fame.txt", "r", encoding = "utf-8")
+  file = open(hall_of_fame_location, "r", encoding = "utf-8")
   hallList = []
-  for line in file :
+  for line in file : # add each entry in the hall of fame to a list
     line = line.replace("\n", "").split(",")
     if line != "" :
-      line[1] = int(line[1].replace(" hours", ""))
+      line[1] = int(line[1].replace(" hours", "")) # to compare hours as integers, we have the remove the hours prefix at each entry
       hallList.append(line)
-  print(hallList)
   file.close()
+
   heroHP = 3000
   heroSpeed = 20
   heroHPLose = 10
@@ -138,54 +142,55 @@ def main() :
   pegasusSpeed = 50
   pegasusHPLose = 15
   hours = 0
-  quest = True
-  home = False
-  stringTravel = "How do you want to travel?(Foot/Pegasus) "
-  stringHome = "How do you want to go home?(Foot/Pegasus) "
-  stats = [" TaskName ", " ByFootDistance ", " ByPegasus ", " HPNeeded "] # so i can get lengths of each stat column later
-  hallstats = [" | Name           |", " | Finish Time "]
-  hall_of_fame_repeat = 3
-  print("Welcome to Hero’s 5 Labors!")
+  quest = True # to input into askTravel(), makes it easier to see where the hero is going
+  home = False # to input into askTravel(), makes it easier to see where the hero is going
+  stringTravel = "How do you want to travel?(Foot/Pegasus) " #string to ask while going on a task
+  stringHome = "How do you want to go home?(Foot/Pegasus) " #string to ask while returning home
+  stats = [" TaskName ", " ByFootDistance ", " ByPegasus ", " HPNeeded "] # so i can get lengths of each stat column
+  hallstats = [" | Name           |", " | Finish Time "] # to get lengths of hall of fame table columns
+  hall_of_fame_repeat = 3 # how many players to show on the hall of fame
 
+  print("Welcome to Hero’s 5 Labors!") # starting text
+  # game loop begins here
   while True :
-    if task_list != [] :
+    if task_list != [] : #if tasks are not over, go with the usual flow of the game
       printStatus(task_list, stats, heroHP, pegasusHP)
-    else :
+    else : # if tasks are over, take name as an input, add them to the hall of fame, print the hall of fame and quit
       print("Congratulations, you have completed the task.")
       name = str(input("What is your name : "))
-      hallList.append([name, hours])
-      hallList = sortList(hallList)
+      hallList.append([name, hours]) #add entry
+      hallList = sortList(hallList) #sort the hall of fame with hours ascending
       for score in hallList :
-        score[1] = str(score[1]) + " hours"
+        score[1] = str(score[1]) + " hours" # add the hours prefix to each entry
       hallString = ""
       file = open("hall_of_fame.txt", "w", encoding = "utf-8")
       for line in hallList :
         hallString += ",".join(line) + "\n"
-      file.write(hallString)
+      file.write(hallString) # write the new hall of fame to the file
       file.close()
-      printHall(hallstats, hallList, 0, True, hall_of_fame_repeat)
+      printHall(hallstats, hallList, 0, True, hall_of_fame_repeat) #print the hall of fame
       break
-
-    destination = askDestination(task_dict, heroHP)
-    if destination == "heaven" :
+    # game phase one where you ask the user to go on a quest
+    destination = askDestination(task_dict, heroHP) # ask where to go
+    if destination == "heaven" : # a silly way to determine if the game is over
       print("Game over.")
       break
-    travel = askTravel(task_dict, destination, heroHP, heroSpeed, heroHPLose, pegasusHP, pegasusSpeed, pegasusHPLose, stringTravel)
-    if travel == "dead" :
+    travel = askTravel(task_dict, destination, heroHP, heroSpeed, heroHPLose, pegasusHP, pegasusSpeed, pegasusHPLose, stringTravel) # ask how to go to the task
+    if travel == "dead" or destination == "heaven" : # a silly way to determine if the game is over
       print("Game over.")
       break
     timeElapsed = calculateTime(task_dict, destination, travel, heroSpeed, pegasusSpeed)
-    heroHP, pegasusHP = calculateHP(task_dict, destination, travel, quest, heroHP, pegasusHP, pegasusHPLose, heroHPLose, timeElapsed)
+    heroHP, pegasusHP = calculateHP(task_dict, destination, travel, quest, heroHP, pegasusHP, pegasusHPLose, heroHPLose, timeElapsed) # calculate hp and time
     hours += timeElapsed
-    if heroHP <= 0  :
+    if heroHP < 0  : # if the hero has less than 0 hp, end the game
       print("Game over.")
       break
     print("Hero defeated the monster.")
     print("Time passed: " + str(hours) + " hours\n")
     printHP(heroHP, pegasusHP)
-
-    travel = askTravel(task_dict, destination, heroHP, heroSpeed, heroHPLose, pegasusHP, pegasusSpeed, pegasusHPLose, stringHome)
-    if travel == "dead" :
+    # game phase two where you return home
+    travel = askTravel(task_dict, destination, heroHP, heroSpeed, heroHPLose, pegasusHP, pegasusSpeed, pegasusHPLose, stringHome) #ask how to return home
+    if travel == "dead" or heroHP < 0 :
       print("Game over.")
       break
     timeElapsed = calculateTime(task_dict, destination, travel, heroSpeed, pegasusSpeed)
@@ -195,5 +200,6 @@ def main() :
     print("Time passed: " + str(hours) + " hours\n")
     
     removeTask(task_list, task_dict, destination, 0)
+    # game loop ends here
 
 main()
